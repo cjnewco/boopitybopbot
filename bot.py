895af058d34c42c31281e114bec6043c589c7a8a
KevQLee <kevin5478233@gmail.com>
2021-12-19 20:16:07 -0600

    embed stuff

--- bot.py
+++ bot.py
@@ -4,9 +4,9 @@ from discord.ext import commands
 from discord.ext.commands import Bot
 from discord.voice_client import VoiceClient
 
-intents = discord.Intents.default()
-intents.members = True
-client = commands.Bot(command_prefix = '$', intents=intents)
+#intents = discord.Intents.default()
+#intents.members = True
+client = commands.Bot(command_prefix = '$') #add intents=intents when ready
 
 # find a way to make it so that this doesnt need to be in the repo lol
 TOKEN = "ODE2ODk5OTQ1MjEzODUzNzU2.YEBrXA.l6cSmHJYrzaLM5TQCUJCWt1hDrQ"
@@ -15,9 +15,9 @@ TOKEN = "ODE2ODk5OTQ1MjEzODUzNzU2.YEBrXA.l6cSmHJYrzaLM5TQCUJCWt1hDrQ"
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
     #update server list in csv here
-    guild = await client.fetch_guild('Volskaya Industries')
-    for member in guild.members:
-        print(member)
+    #guild = await client.get_guild('Volskaya Industries')
+    #for member in guild.members:
+    #    print(member)
 
 @client.command()
 async def meow(ctx):
@@ -27,7 +27,7 @@ async def meow(ctx):
     vc.play(discord.FFmpegPCMAudio('meow.mp3'), after=lambda e: print('done', e))
     
 @client.command() 
-async def startgamba(ctx, *, nameofbet):
+async def startgamba(ctx, *, nameofgamba):
     emojis = ['👍','👎']
     msg = await ctx.send(nameofbet)
     for emoji in emojis:
@@ -44,8 +44,19 @@ async def play( ctx, *, arg ):
 
 @client.command()
 async def startbet(ctx, *, nameofbet):
-    msg = await ctx.send(nameofbet)
-    
+    channel = ctx.message.channel
+    emb = discord.Embed(
+        title = 'Title', 
+        description = 'This is a description', 
+        colour = discord.Colour.blue()
+        )
+    emb.set_footer(text='This is a footer')
+    emb.set_image(url='https://cdn.discordapp.com/attachments/412061704759934987/922281923840213002/5315.png')
+    emb.set_thumbnail(url='https://cdn.discordapp.com/attachments/412061704759934987/922281923840213002/5315.png')
+    emb.set_author(name = 'Author name', icon_url = 'https://cdn.discordapp.com/attachments/412061704759934987/922281923840213002/5315.png')
+    emb.add_field(name = 'Field Name', value = 'Field Value', inline = True)
+    await ctx.send(embed=emb)
+    await client.send_message(channel, embed=emb)
 
 
 client.run(TOKEN)
